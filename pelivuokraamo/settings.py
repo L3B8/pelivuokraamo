b2347a5624c469a2b5dc3787b62ac510369585e0
kysoosi <samidemus@gmail.com>
2025-04-21 19:39:42 +0300

    Lisätty kommmentit setting.py MEDIA_URL ja MEDIA_ROOT

--- pelivuokraamo/settings.py
+++ pelivuokraamo/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# TODO: Tänne pitäisi määritellä polku cover_image tiedostoille
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media' nämä tuli github copilotilta. Leevi tarkista miten nämä pitäisi määritellä :D t. Sami
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
