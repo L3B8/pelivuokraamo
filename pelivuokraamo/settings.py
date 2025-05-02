8ea69a569edf8d817c448e5095d26da891dd134d
Ubuntu <LeeviN@myVm.jglfhygsonrediszeuzz5ajsxa.fx.internal.cloudapp.net>
2025-05-02 23:21:55 +0000

    Initial full commit

--- pelivuokraamo/settings.py
+++ pelivuokraamo/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -23,8 +22,7 @@ SECRET_KEY = 'django-insecure-tn$bgm!=r4wx!(*02jvo%*eg39*t^c+ytd!2hk5=a(6@0m0gbg
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['72.145.8.199']
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -118,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # TODO: Tänne pitäisi määritellä polku cover_image tiedostoille
 # MEDIA_URL = '/media/'
